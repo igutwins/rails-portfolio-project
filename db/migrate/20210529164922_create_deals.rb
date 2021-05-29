b9fb458795faf65ca0c4a663ea47d9bb8244a73a
igutwins <iangutwinski@gmail.com>
2021-05-29 12:50:45 -0400

    added deals migration

---
+++ db/migrate/20210529164922_create_deals.rb
@@ -0,0 +1,15 @@
+class CreateDeals < ActiveRecord::Migration[6.0]
+  def change
+    create_table :deals do |t|
+      t.string :name
+      t.decimal :entry_cash
+      t.decimal :interim_growth
+      t.decimal :terminal_growth
+      t.decimal :discount_rate
+      t.decimal :npv
+      t.string :industry
+
+      t.timestamps
+    end
+  end
+end
